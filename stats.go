38cfe9b217bea3173de1b3bfddccf61b70a3a7ad
jacexh <jace@xuh.me>
2017-02-24 14:45:34 +0800

    percentile

--- stats.go
+++ stats.go
@@ -1,9 +1,11 @@
 package ultron
 
 import (
+	"sort"
 	"sync"
-	"sync/atomic"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 type (
@@ -12,17 +14,17 @@ type (
 
 	// StatsEntry represents a single stats entry
 	StatsEntry struct {
-		Name              string
-		NumRequests       uint64
-		NumFailures       uint64
-		TotalResponseTime time.Duration
-		MinResponseTime   time.Duration
-		MaxResponseTime   time.Duration
-		TPSTrend          map[int64]uint64
-		ResponseTimes     map[RoundedMillisecond]uint64
-		StartTime         time.Time
-		LastRequestTime   time.Time
-		interval          time.Duration // interval 统计TPS的时间间隔
+		Name              string                     // 事务名称
+		NumRequests       int                        // 成功请求次数
+		NumFailures       int                        // 失败次数
+		TotalResponseTime time.Duration              // 成功请求的response time总和，基于原始的响应时间
+		MinResponseTime   time.Duration              // 最快的响应时间
+		MaxResponseTime   time.Duration              // 最慢的响应时间
+		Trend             map[int64]int              // 按时间轴（秒级）记录成功请求次数
+		ResponseTimes     map[RoundedMillisecond]int // 按优化后的响应时间记录成功请求次数
+		StartTime         time.Time                  // 第一次收到请求的时间
+		LastRequestTime   time.Time                  // 最后一次收到请求的时间
+		interval          time.Duration              // 统计时间间隔，影响 CurrentQPS()
 		lock              *sync.RWMutex
 	}
 
@@ -30,7 +32,7 @@ type (
 	StatsCollector map[string]*StatsEntry
 )
 
-func timeDurationToRoudedMilliSecond(t time.Duration) RoundedMillisecond {
+func timeDurationToRoudedMillisecond(t time.Duration) RoundedMillisecond {
 	ms := int64(t.Seconds()*1000 + 0.5)
 	var rm RoundedMillisecond
 	if ms < 100 {
@@ -43,12 +45,16 @@ func timeDurationToRoudedMilliSecond(t time.Duration) RoundedMillisecond {
 	return rm
 }
 
+func roundedMillisecondToDuration(r RoundedMillisecond) time.Duration {
+	return time.Duration(r * 1000 * 1000)
+}
+
 // NewStatsEntry create a new StatsEntry instance
 func NewStatsEntry(n string) *StatsEntry {
 	return &StatsEntry{
 		Name:          n,
-		TPSTrend:      map[int64]uint64{},
-		ResponseTimes: map[RoundedMillisecond]uint64{},
+		Trend:         map[int64]int{},
+		ResponseTimes: map[RoundedMillisecond]int{},
 		interval:      time.Second * 2,
 		lock:          &sync.RWMutex{},
 	}
@@ -58,10 +64,11 @@ func (s *StatsEntry) logSuccess(t time.Duration) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	atomic.AddUint64(&s.NumRequests, 1)
+	s.NumRequests++
 
 	now := time.Now()
 	if s.LastRequestTime.IsZero() {
+		s.MinResponseTime = t
 		s.StartTime = now
 	}
 	s.LastRequestTime = now
@@ -75,13 +82,13 @@ func (s *StatsEntry) logSuccess(t time.Duration) {
 	s.TotalResponseTime += t
 
 	sec := now.Unix()
-	if _, ok := s.TPSTrend[sec]; ok {
-		s.TPSTrend[sec]++
+	if _, ok := s.Trend[sec]; ok {
+		s.Trend[sec]++
 	} else {
-		s.TPSTrend[sec] = 1
+		s.Trend[sec] = 1
 	}
 
-	rm := timeDurationToRoudedMilliSecond(t)
+	rm := timeDurationToRoudedMillisecond(t)
 	if _, ok := s.ResponseTimes[rm]; ok {
 		s.ResponseTimes[rm]++
 	} else {
@@ -93,10 +100,45 @@ func (s *StatsEntry) logFailure(t time.Duration, e error) {
 
 }
 
-// TotalTPS 获取总的TPS
-func (s *StatsEntry) TotalTPS() float64 {
+// TotalQPS 获取总的QPS
+func (s *StatsEntry) TotalQPS() float64 {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
 
 	return float64(s.NumRequests) / s.LastRequestTime.Sub(s.StartTime).Seconds()
+}
+
+// Percentile 获取x%的响应时间
+func (s *StatsEntry) Percentile(f float64) time.Duration {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	if f <= 0.0 {
+		return s.MinResponseTime
+	}
+
+	if f >= 1.0 {
+		return s.MaxResponseTime
+	}
+
+	hint := int(float64(s.NumRequests)*f + .5)
+	if hint == s.NumRequests {
+		return s.MaxResponseTime
+	}
+
+	var times []RoundedMillisecond
+	for k := range s.ResponseTimes {
+		times = append(times, k)
+	}
+
+	sort.Slice(times, func(i, j int) bool { return times[i] < times[j] })
+	for _, val := range times {
+		counts := s.ResponseTimes[val]
+		hint -= counts
+		if hint <= 0 {
+			return roundedMillisecondToDuration(val)
+		}
+	}
+	Logger.Warn("occer error", zap.Int("hint", hint))
+	return time.Nanosecond // occur error
 }
--- stats_test.go
+++ stats_test.go
@@ -14,23 +14,23 @@ func TestNewStatsEntry(t *testing.T) {
 
 func TestRoudedMilliSecond(t *testing.T) {
 	d := time.Millisecond * 54
-	rd := timeDurationToRoudedMilliSecond(d)
+	rd := timeDurationToRoudedMillisecond(d)
 	assert.True(t, (rd == 54), "not rounded")
 
 	d = time.Millisecond * 124
-	rd = timeDurationToRoudedMilliSecond(d)
+	rd = timeDurationToRoudedMillisecond(d)
 	assert.True(t, (rd == 120), "equal 120")
 
 	d = time.Millisecond * 125
-	rd = timeDurationToRoudedMilliSecond(d)
+	rd = timeDurationToRoudedMillisecond(d)
 	assert.True(t, (rd == 130), "equal 130")
 
 	d = time.Millisecond * 1230
-	rd = timeDurationToRoudedMilliSecond(d)
+	rd = timeDurationToRoudedMillisecond(d)
 	assert.True(t, (rd == 1200), "equal 1200")
 
 	d = time.Millisecond * 1250
-	rd = timeDurationToRoudedMilliSecond(d)
+	rd = timeDurationToRoudedMillisecond(d)
 	assert.True(t, (rd == 1300), "equal 1300")
 }
 
@@ -41,5 +41,21 @@ func TestLogSuccess(t *testing.T) {
 	}
 	time.Sleep(time.Second * 1)
 	s.logSuccess(time.Millisecond * 84)
-	assert.True(t, (s.TotalTPS() > 9900.0), "greater than 9900")
+	assert.True(t, (s.TotalQPS() > 9900.0), "greater than 9900")
+}
+
+func TestPercentile(t *testing.T) {
+	s := NewStatsEntry("test")
+
+	s.logSuccess(time.Millisecond * 10)
+	s.logSuccess(time.Millisecond * 20)
+	s.logSuccess(time.Millisecond * 20)
+	s.logSuccess(time.Millisecond * 30)
+	s.logSuccess(time.Millisecond * 50)
+
+	assert.Equal(t, time.Millisecond*10, s.Percentile(0))
+	assert.Equal(t, time.Millisecond*20, s.Percentile(.4))
+	assert.Equal(t, time.Millisecond*50, s.Percentile(1))
+	assert.Equal(t, time.Millisecond*30, s.Percentile(.8))
+	assert.Equal(t, time.Millisecond*50, s.Percentile(.99))
 }
