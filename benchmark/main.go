cafe9d212e5a1c271bff37112de5ea034c7d4f55
jacexh <jace@xuh.me>
2017-04-21 13:43:30 +0800

    add benchmark test

---
+++ benchmark/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/jacexh/ultron"
+)
+
+type BenchmarkAttacher struct {
+	taskset *ultron.TaskSet
+}
+
+func (b *BenchmarkAttacher) Name() string {
+	return "benchmark"
+}
+
+func (b *BenchmarkAttacher) TaskSet() *ultron.TaskSet {
+	return b.taskset
+}
+
+func (b *BenchmarkAttacher) SetTaskSet(s *ultron.TaskSet) {
+	b.taskset = s
+}
+
+func (b *BenchmarkAttacher) Fire() (int, error) {
+	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(11)))
+	return 0, nil
+}
+
+func main() {
+	attacker := &BenchmarkAttacher{}
+
+	tasks := ultron.NewTaskSet()
+	tasks.Add(attacker, 1)
+	tasks.MinWait = ultron.ZeroDuration
+	tasks.MaxWait = ultron.ZeroDuration
+
+	ultron.Runner.Config.Concurrence = 10000
+
+	ultron.Runner.Run(tasks)
+}
