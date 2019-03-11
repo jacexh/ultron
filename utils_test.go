81a612449a53490b469ba8caed7b4d48723a9649
Richard1ybb <yangtianyangxu@qq.com>
2019-03-11 16:37:58 +0800

    add test case for func abs

--- utils_test.go
+++ utils_test.go
@@ -1,5 +1,7 @@
 package ultron
 
+import "testing"
+
 func ExampleShowLogo() {
 	ShowLogo()
 	//Output:
@@ -9,4 +11,25 @@ func ExampleShowLogo() {
 	//\ \_/ /| || |_ | |   | (_) || | | |
 	//  \___/ |_| \__||_|    \___/ |_| |_|
 
+}
+
+func TestAbs(t *testing.T) {
+	type testData struct{
+		in   int
+		want int
+	}
+
+	var tds = []testData{
+		{0, 0},
+		{1, 1},
+		{-1,1},
+		{-999, 999},
+		{999, 999},
+	}
+
+	for _, td := range tds {
+		if got := Abs(td.in); td.want != got {
+			t.Errorf("Abs(): got: %s, want: %s", got, td.want)
+		}
+	}
 }
