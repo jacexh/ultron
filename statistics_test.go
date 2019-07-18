4542cf9cf72a07635ffc13701ced134ee15b9082
jacexh <jace.xh@gmail.com>
2019-07-18 21:33:47 +0800

    #4 添加部分用例

---
+++ statistics_test.go
@@ -0,0 +1,72 @@
+package ultron
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewLimitedSizeMap(t *testing.T) {
+	m := newLimitedSizeMap(3)
+	assert.EqualValues(t, 3, m.size)
+}
+
+func TestLimitedSizeMap_accumulateOK(t *testing.T) {
+	m := newLimitedSizeMap(3)
+	m.accumulate(100, 1)
+	assert.EqualValues(t, 1, m.content[100])
+
+	m.accumulate(100, 3)
+	assert.EqualValues(t, 4, m.content[100])
+}
+
+func TestLimitedSizeMap_accumulateToRemoveEle(t *testing.T) {
+	m := newLimitedSizeMap(3)
+	m.accumulate(100, 1)
+	assert.EqualValues(t, 1, m.content[100])
+	assert.EqualValues(t, 1, len(m.content))
+
+	m.accumulate(103, 1)
+	assert.EqualValues(t, 2, len(m.content))
+
+	m.accumulate(104, 1)
+	assert.EqualValues(t, 2, len(m.content))
+	_, ok := m.content[100]
+	assert.False(t, ok)
+}
+
+func TestNewAttackerStatistics(t *testing.T) {
+	s := newAttackerStatistics("foobar")
+	assert.Equal(t, s.name, "foobar")
+	assert.Equal(t, s.interval, 12*time.Second)
+	assert.EqualValues(t, s.trendFailures.size, 20)
+	assert.EqualValues(t, s.trendSuccess.size, 20)
+	assert.True(t, s.lastRequestTime.IsZero())
+	assert.True(t, s.startTime.IsZero())
+}
+
+func TestAttackerStatistics_logSuccess(t *testing.T) {
+	name := "foobar"
+	stats := newAttackerStatistics(name)
+
+	ret := &Result{Name: name, Duration: int64(10 * time.Millisecond)}
+	stats.logSuccess(ret)
+
+	assert.False(t, stats.startTime.IsZero())
+	assert.False(t, stats.lastRequestTime.IsZero())
+	assert.EqualValues(t, stats.minResponseTime, 10*time.Millisecond)
+	assert.EqualValues(t, stats.minResponseTime, stats.maxResponseTime)
+	assert.EqualValues(t, stats.totalResponseTime, 10*time.Millisecond)
+	assert.EqualValues(t, stats.numRequests, 1)
+
+	ret = &Result{Name: name, Duration: int64(20 * time.Millisecond)}
+	time.Sleep(time.Second)
+	stats.log(ret)
+	assert.True(t, stats.lastRequestTime.After(stats.startTime))
+	assert.EqualValues(t, stats.maxResponseTime, 20*time.Millisecond)
+	assert.EqualValues(t, stats.minResponseTime, 10*time.Millisecond)
+	assert.EqualValues(t, stats.totalResponseTime, 30*time.Millisecond)
+	assert.EqualValues(t, stats.numRequests, 2)
+	assert.EqualValues(t, 2, len(stats.trendSuccess.content))
+}
