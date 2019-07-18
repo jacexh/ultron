3ae49adbeff73cf46c49a0dd5ccfadc6b740c406
jacexh <jace.xh@gmail.com>
2019-07-18 15:05:23 +0800

    add some cases

--- attacker_test.go
+++ attacker_test.go
@@ -16,6 +16,14 @@ type (
 	mockPrepareFunc struct {
 		mock.Mock
 	}
+
+	mockHTTPChecker struct {
+		mock.Mock
+	}
+
+	mockFastClient struct {
+		mock.Mock
+	}
 )
 
 func (ep *mockPrepareFunc) fastHTTPPrepare(req *fasthttp.Request) error {
@@ -23,9 +31,34 @@ func (ep *mockPrepareFunc) fastHTTPPrepare(req *fasthttp.Request) error {
 	return args.Error(0)
 }
 
-func (ep *mockPrepareFunc) httpPrepare() (*http.Request, error) {
+func (ep *mockPrepareFunc) httpPrepare() (req *http.Request, err error) {
 	args := ep.Called()
 	return args.Get(0).(*http.Request), args.Error(1)
+}
+
+func (hc *mockHTTPChecker) httpChecker(res *http.Response, d []byte) error {
+	args := hc.Called(res, d)
+	return args.Error(0)
+}
+
+func (hc *mockHTTPChecker) fastHTTPChecker(res *fasthttp.Response) error {
+	args := hc.Called(res)
+	return args.Error(0)
+}
+
+func (mc *mockFastClient) Do(req *fasthttp.Request, res *fasthttp.Response) error {
+	args := mc.Called(req, res)
+	return args.Error(0)
+}
+
+func (mc *mockFastClient) DoDeadline(req *fasthttp.Request, res *fasthttp.Response, t time.Time) error {
+	args := mc.Called(req, res, t)
+	return args.Error(0)
+}
+
+func (mc *mockFastClient) DoTimeout(req *fasthttp.Request, res *fasthttp.Response, d time.Duration) error {
+	args := mc.Called(req, res, d)
+	return args.Error(0)
 }
 
 func TestNewFastHTTPAttacker(t *testing.T) {
@@ -56,6 +89,41 @@ func TestFastHTTPAttacker_Fire_PrepareError(t *testing.T) {
 	assert.EqualError(t, attacker.Fire(), "bad prepare func")
 }
 
+func TestFastHTTPAttacker_Fire(t *testing.T) {
+	client := new(mockFastClient)
+	client.On("Do", &fasthttp.Request{}, &fasthttp.Response{}).Return(nil)
+	attacker := NewFastHTTPAttacker(
+		"foobar",
+		client,
+		func(request *fasthttp.Request) error { return nil },
+	)
+	assert.Nil(t, attacker.Fire())
+
+	client = new(mockFastClient)
+	attacker = NewFastHTTPAttacker(
+		"foobar",
+		client,
+		func(request *fasthttp.Request) error { return nil },
+	)
+	client.On("Do", &fasthttp.Request{}, &fasthttp.Response{}).Return(errors.New("fire failure"))
+	assert.EqualError(t, attacker.Fire(), "fire failure")
+}
+
+func TestFastHTTPAttacker_CheckError(t *testing.T) {
+	client := new(mockFastClient)
+	checker := new(mockHTTPChecker)
+
+	client.On("Do", &fasthttp.Request{}, &fasthttp.Response{}).Return(nil)
+	checker.On("fastHTTPChecker", &fasthttp.Response{}).Return(errors.New("check failure"))
+	attacker := NewFastHTTPAttacker(
+		"foobar",
+		client,
+		func(request *fasthttp.Request) error { return nil },
+		checker.fastHTTPChecker,
+	)
+	assert.EqualError(t, attacker.Fire(), "check failure")
+}
+
 func TestNewHTTPAttacker(t *testing.T) {
 	name := "hello world"
 	attacker := NewHTTPAttacker(name, nil)
@@ -70,6 +138,7 @@ func TestHTTPAttacker_Name(t *testing.T) {
 	attacker := NewHTTPAttacker(name, nil)
 	assert.NotNil(t, attacker)
 	assert.Equal(t, name, attacker.Name())
+	assert.Equal(t, attacker.Client, DefaultHTTPClient)
 }
 
 func TestHTTPAttacker_Fire_NoPrepare(t *testing.T) {
@@ -78,8 +147,7 @@ func TestHTTPAttacker_Fire_NoPrepare(t *testing.T) {
 
 func TestHTTPAttacker_Fire_PrepareError(t *testing.T) {
 	p := new(mockPrepareFunc)
-	req, _ := http.NewRequest(http.MethodGet, "http://www.baidu.com", nil)
-	p.On("httpPrepare").Return(req, errors.New("bad prepare func"))
+	p.On("httpPrepare").Return(new(http.Request), errors.New("bad prepare func"))
 	attacker := NewHTTPAttacker("hello", p.httpPrepare)
 	assert.EqualError(t, attacker.Fire(), "bad prepare func")
 }
