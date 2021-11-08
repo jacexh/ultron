30548c8cc8287d216ca5c1c49fd8d44a7ce73cb0
jacexh <jace.xh@gmail.com>
2021-11-08 06:56:09 +0000

    update

--- eventbus_test.go
+++ eventbus_test.go
@@ -2,9 +2,11 @@ package ultron
 
 import (
 	"context"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/wosai/ultron/v2/pkg/statistics"
 )
 
@@ -25,4 +27,34 @@ func BenchmarkIEventBus_PublishResult(b *testing.B) {
 			bus.publishResult(statistics.AttackResult{Name: "benchmark", Duration: 100 * time.Millisecond})
 		}
 	})
+}
+
+func TestEventBus_report(t *testing.T) {
+	eb := newEventBus()
+	eb.start()
+
+	var called int32
+	eb.subscribeReport(func(c context.Context, sr statistics.SummaryReport) {
+		atomic.AddInt32(&called, 1)
+	})
+	eb.publishReport(statistics.SummaryReport{})
+
+	eb.close()
+	current := atomic.LoadInt32(&called)
+	assert.EqualValues(t, current, 1)
+}
+
+func TestEventBus_result(t *testing.T) {
+	eb := newEventBus()
+	eb.start()
+
+	var called int32
+	eb.subscribeResult(func(c context.Context, ar statistics.AttackResult) {
+		atomic.AddInt32(&called, 1)
+	})
+	eb.publishResult(statistics.AttackResult{Name: "unittest", Duration: 10 * time.Millisecond})
+	eb.close()
+
+	current := atomic.LoadInt32(&called)
+	assert.EqualValues(t, current, 1)
 }
