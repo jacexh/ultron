9ac1362ae06615d4ee4c41298eb2ef2fdb235430
jacexh <jace.xh@gmail.com>
2021-11-19 01:55:56 +0000

    补充salve agent的测试用例

--- slave_agent_test.go
+++ slave_agent_test.go
@@ -1,10 +1,13 @@
 package ultron
 
 import (
+	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/wosai/ultron/v2/pkg/genproto"
 	"go.uber.org/zap"
 )
@@ -19,9 +22,34 @@ func TestSlaveAgent_close(t *testing.T) {
 			defer wg.Done()
 			agent.send(&genproto.SubscribeResponse{Type: genproto.EventType_PING})
 			if err := agent.close(); err != nil {
-				Logger.Info("no consumer for the slave agent", zap.Error(err))
+				Logger.Error("no consumer for the slave agent", zap.Error(err))
 			}
 		}()
 	}
 	wg.Wait()
+}
+
+func TestSlaveAgent_ID(t *testing.T) {
+	agent := newSlaveAgent(&genproto.SubscribeRequest{SlaveId: uuid.NewString()})
+	assert.True(t, agent.ID() != "")
+
+	extras := agent.Extras()
+	assert.EqualValues(t, len(extras), 0)
+}
+
+func TestSlaveAgent_closeTimeout(t *testing.T) {
+	agent := newSlaveAgent(&genproto.SubscribeRequest{SlaveId: uuid.NewString()})
+	agent.send(&genproto.SubscribeResponse{})
+	err := agent.close()
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.Error(), fmt.Sprintf("the input channel is blocked: %s", agent.ID()))
+}
+
+func TestSlaveAgent_keepAliveOut(t *testing.T) {
+	agent := newSlaveAgent(&genproto.SubscribeRequest{SlaveId: uuid.NewString()})
+	go func() {
+		<-time.After(1 * time.Second)
+		agent.close()
+	}()
+	agent.keepAlives()
 }
