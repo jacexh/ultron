cdbe046ab4ef83485be657b168ba05b28af3a819
dongmeijuan <1398670158@qq.com>
2021-12-01 16:56:56 +0800

    更新版本

--- front/.umirc.ts
+++ front/.umirc.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'umi';
+import { defineConfig } from "umi";
 const assetDir = "static";
 
 export default defineConfig({
   nodeModulesTransform: {
-    type: 'none',
+    type: "none",
   },
-  routes: [{ path: '/', component: '@/pages/ultronHome/index' }],
+  outputPath: "../web",
+  publicPath: "./",
+  history: { type: "hash" },
+  routes: [{ path: "/", component: "@/pages/ultronHome/index" }],
+
   chainWebpack: (config) => {
     //修改JS输出目录
     config.output
-      .filename(assetDir + '/[name].[hash:8].js')
-      .chunkFilename(assetDir + '/[name].[contenthash:8].chunk.js');
+      .filename(assetDir + "/[name].[hash:8].js")
+      .chunkFilename(assetDir + "/[name].[contenthash:8].chunk.js");
     // 修改css输出目录
-    config.plugin('extract-css').tap(() => [
+    config.plugin("extract-css").tap(() => [
       {
         filename: assetDir + `/[name].[contenthash:8].css`,
         chunkFilename: assetDir + `/[name].[contenthash:8].chunk.css`,
@@ -20,19 +24,20 @@ export default defineConfig({
       },
     ]);
   },
+
   proxy: {
-    '/api': {
-      target: 'http://127.0.0.1:2017',
+    "/api": {
+      target: "http://127.0.0.1:2017",
       changeOrigin: true,
       pathRewrite: {
-        '^/server': '',
+        "^/server": "",
       },
     },
-    '/metrics': {
-      target: 'http://127.0.0.1:2017',
+    "/metrics": {
+      target: "http://127.0.0.1:2017",
       changeOrigin: true,
       pathRewrite: {
-        '^/server': '',
+        "^/server": "",
       },
     },
   },
